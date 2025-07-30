3760430398d4ae30ad964420a3f474bc8cff99da
billchetch <45006134+billchetch@users.noreply.github.com>
2025-07-31 00:30:47 +0800

    asdf

---
+++ src/devices/comms/ChetchMCP2515Device.cpp
@@ -0,0 +1,115 @@
+#include "ChetchUtils.h"
+#include "ChetchMCP2515Device.h"
+
+
+namespace Chetch{
+    MCP2515Device::MCP2515Device(byte busID, byte nodeID, int csPin) : mcp2515(csPin) {
+       busAndNodeID = busID << 5 | (0b0011111 & nodeID);
+    }
+
+    bool MCP2515Device::begin(){
+        mcp2515.reset();
+        mcp2515.setBitrate(CAN_125KBPS);
+#if CAN_AS_LOOPBAck 
+        mcp2515.setLoopbackMode();
+#else
+        mcp2515.setNormalMode();
+#endif
+	}
+    
+    void MCP2515Device::loop(){
+        ArduinoDevice::loop();
+
+        MCP2515::ERROR err = mcp2515.readMessage(&canInFrame);
+        switch(err){
+            case MCP2515::ERROR_OK:
+                if(messageReceivedListener != NULL){
+                    messageReceivedListener(this, NULL);
+                }
+                Serial.print("Received: ");
+                Serial.println(canInFrame.can_dlc);
+                break;
+
+            default:
+                //Serial.println("Received something weird");
+                break;
+        }
+
+    }
+
+    bool MCP2515Device::sendMessage(ArduinoMessage* message){
+        if(message->getArgumentCount() > 4){
+            return false; //ERROR
+        }
+        canOutFrame.can_dlc = 0;
+        byte shiftLeft = 6;
+        byte messageStructure = message->getArgumentCount() - 1 << shiftLeft;
+        for(int i = 0; i < message->getArgumentCount(); i++){
+            if(message->getArgumentSize(i) > 4){
+                return false; //ERROR
+            }
+            canOutFrame.can_dlc += message->getArgumentSize(i);
+            if(canOutFrame.can_dlc > 8){
+                return false; //ERROR
+            }
+            if(i < 3){
+                shiftLeft -= 2;
+                messageStructure = messageStructure | (message->getArgumentSize(i) - 1 << shiftLeft);
+            }
+        }
+        /*Serial.print("CAN dlc ");
+        Serial.println(canOutFrame.can_dlc);
+        Serial.print("Bits of the byte: ");
+        for (int i = 7; i >= 0; i--) { // Loop from bit 7 (MSB) down to 0 (LSB)
+            Serial.print(bitRead(messageStructure, i)); // Print the value of the i-th bit
+        }        
+        return true;*/
+
+        byte messageTypeAndSenderID;
+        switch(message->type){
+            case ArduinoMessage::TYPE_ERROR:
+                messageTypeAndSenderID = 0b00100000 | message->sender;
+                break;
+            case ArduinoMessage::TYPE_DATA:
+                messageTypeAndSenderID = 0b01000000 | message->sender;;
+                break;
+            default:
+                messageTypeAndSenderID = 0b00000000 | message->sender;;
+                break;
+        }
+        canOutFrame.can_id = (unsigned long)busAndNodeID << 16 | messageTypeAndSenderID << 8 | messageStructure;
+        /*Serial.println("Bits of the byte: ");
+        for (int i = (sizeof(canOutFrame.can_id) * 8) - 1; i >= 0; i--) { // Loop from bit 7 (MSB) down to 0 (LSB)
+            Serial.print(bitRead(canOutFrame.can_id, i)); // Print the value of the i-th bit
+            if(i % 8 == 0)Serial.println("----");
+        }  
+        Serial.println("----");*/
+        
+        
+        canOutFrame.can_id |= CAN_EFF_FLAG;
+        int n = 0;
+        for(int i = 0; i < message->getArgumentCount(); i++){
+            byte* b = message->getArgument(i);
+            for(int j = 0; j < message->getArgumentSize(i); j++){
+                canOutFrame.data[n++] = b[j];
+            }
+        }
+
+        if(mcp2515.sendMessage(&canOutFrame) == MCP2515::ERROR_OK){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool MCP2515Device::executeCommand(DeviceCommand command, ArduinoMessage *message, ArduinoMessage *response){
+        bool handled = ArduinoDevice::executeCommand(command, message, response);
+        
+        if(!handled)
+        {
+            //switch(command){
+            //}
+        }
+        return handled;
+    }
+}
---
+++ src/devices/comms/ChetchMCP2515Device.h
@@ -0,0 +1,99 @@
+#include <Arduino.h>
+
+#include <ChetchArduinoDevice.h>
+#include <ChetchArduinoMessage.h>
+
+
+#include <SPI.h>
+#include <mcp2515.h>
+
+/*
+NANO
+INT PIN = D2 (this is not required if looping)
+CS PIN = D10
+MOSI PIN = D11
+MISO PIN = D12
+SCK PIN = D13
+
+This class is deisgned to:
+
+1. Wrap an MCP2515 device in an ArduinoDevice class so it can be added to an ArduinoBoard
+2. Perform message conversions between ArduinoMessage messages and CAN frames (Extended ID)
+
+Wrapping MCP2515 device
+Currently using the autowp library but this could change...
+
+Message Conversion
+The key to this is using the extended ID (so 29 bit) ID value as follows:
+
+- Byte 1 = 3 bits for numbering the particular bus and 5 bits for the node.  This allows for 8 separate buses each with 32 max nodes.
+- Byte 2 = 3 bits to indicate a type used to translate to the Arduino Message Type and 5 bits to prpresent a sender ID (hence 8 types 32 possible senders
+- Byte 3 = Message structure:  2 bits for argument length, then 2 bits for arg 1 length, 2 bits for arg2 length and 2 bits for arg 3 length.
+
+More on the message structure... in conjuntion with the can frame DLC value, Byte 3 allows for 4 possible arguments with each argument being of max 4 bytes. 
+This corresponds to arduino basic types (float, int, long, byte etc.) It also allows for a varilable length argument if there is only 1 arg as we can use the DLC
+value to determine
+
+Free single argument example:
+DLC = 0 to 8
+Byte 3 = 00 00 00 00 00
+
+2 arguments example (4 bytes and 2 bytes)
+DLC = 6
+Byte 3 = 01 11 01 00
+
+3 arguments example (3 bytes and 4 bytes 1 byte)
+DLC = 6
+Byte 3 = 10 11 01 00
+
+4 arguments example (2 bytes and 3 bytes 1 byte 2 byte)
+DLC = 8
+Byte 3 = 11 01 10 00
+
+Note that 
+*/
+
+#define CAN_AS_LOOPBAck true
+#define CAN_DEFAULT_CS_PIN 10
+
+namespace Chetch{
+    class MCP2515Device : public ArduinoDevice{
+        public:
+            const byte CAN_NETWORK_BITS = 3;
+            const byte CAN_NODE_BITS = 5;
+
+            enum CANMessageType{
+                CAN_TYPE_NONE = 0,
+                CAN_TYPE_ERROR,
+                CAN_TYPE_DATA,
+                CAN_TYPE_OTHER
+            };
+
+            typedef bool (*MessageListener)(MCP2515Device*, ArduinoMessage*);
+            typedef bool (*ErrorListener)(MCP2515Device*, int errorCode);
+            MCP2515 mcp2515;
+            
+        private:
+            
+            byte busAndNodeID = 0;
+            struct can_frame canInFrame;
+            struct can_frame canOutFrame;
+
+            MessageListener messageReceivedListener = NULL;
+            MessageListener messageSentListener = NULL;
+            ErrorListener errorListener = NULL;
+            
+        public:
+            MCP2515Device(byte busID = 0, byte nodeID = 0, int csPin = CAN_DEFAULT_CS_PIN);
+            
+            bool begin() override;
+            void loop() override;
+            bool executeCommand(DeviceCommand command, ArduinoMessage *message, ArduinoMessage *response) override;
+
+            void addMessageReceivedListener(MessageListener listener){ messageReceivedListener = listener; }
+            void addMessageSentListener(MessageListener listener){ messageSentListener = listener; }
+            void addErrorListener(ErrorListener listener){ errorListener = listener; }
+
+            bool sendMessage(ArduinoMessage *message);
+    };
+} //end namespace
