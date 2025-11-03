ad578add90ae9ce00d53166886728fad3a41beb5
billchetch <45006134+billchetch@users.noreply.github.com>
2025-11-03 10:24:42 +0800

    asdf

--- src/devices/comms/ChetchMCP2515Device.h
+++ src/devices/comms/ChetchMCP2515Device.h
@@ -123,9 +123,9 @@ namespace Chetch{
                 FAIL_TX, //TX error
                 ALL_TX_BUSY, //TX error
                 READ_FAIL, //RX error
-                CRC_ERROR, //RX error (probably from SPI issues but who knows.. )
-                STALE_MESSAGE, //an old message
-                SYNC_ERROR, //if presence is out of sync
+                CRC_ERROR, //RX error - probably from SPI issues but who knows.. 
+                STALE_MESSAGE, //RX error - an old message
+                SYNC_ERROR, //RX error - if presence is out of sync
                 CUSTOM_ERROR, //For individual applications
                 DEBUG_ASSERT, //For debug purposes
             };
