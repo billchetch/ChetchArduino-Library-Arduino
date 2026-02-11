e6c2426c42d0bb6fcd7345e0f81073549407f8c5
billchetch <45006134+billchetch@users.noreply.github.com>
2026-02-11 21:52:27 +0700

    asdf

--- src/devices/comms/ChetchSerialPin.h
+++ src/devices/comms/ChetchSerialPin.h
@@ -8,15 +8,37 @@
 #include <ChetchArduinoMessage.h>
 
 namespace Chetch{
-    class SerialPin : public ArduinoDevice{
+    class SerialPin : public ArduinoDevice {
         public:
+            enum Role : byte{
+                MASTER = 1,
+                SLAVE = 2,
+            };
+
+        private: 
+            Role role;
             byte pin = 0;
-                        
+
+            unsigned long lastPinIO = 0;
+            
+            bool ready4comms = false;
+            bool sending = false;
+
+            int interval = 100;
+            byte data = 0;
+            byte bitCount = 0;
+
+        protected:
+            void pinWrite(byte bit);
+            byte pinRead();
+
         public:
-            SerialPin(byte pin);
+            SerialPin(Role role, byte pin);
         
             bool begin() override;
             void loop() override;
+
+            bool send(byte b);
     }; //end class
 
 } //end namespace
